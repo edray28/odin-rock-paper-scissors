84a7414c4d8d1074fe7756b744921fb096449a66
edray28 <edraytuyor@gmail.com>
2022-07-03 16:42:38 +0800

    added gui

--- script.js
+++ script.js
@@ -1,41 +1,61 @@
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return `You Win PLAYER:${playerSelection} beats CPU:${computerSelection}`
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return `You Win PLAYER:${playerSelection} beats CPU:${computerSelection}`
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return `You Win PLAYER:${playerSelection} beats CPU:${computerSelection}`
-    } else if (playerSelection === computerSelection) {
-        return 'DRAW'
-    } else if (playerSelection === computerSelection) {
-        return 'DRAW'
-    }
-    else {
-        return `You Lose CPU:${computerSelection} beats PLAYER: ${playerSelection}`
-    }
-}
-function computerPlay() {
-    const choices = Math.floor(Math.random(3) * 3) + 1
-    if (choices == 1) {
-        return 'Rock'.toLowerCase()
-    } else if (choices == 2) {
-        return 'Paper'.toLowerCase()
-    } else if (choices == 3) {
-        return 'Scissors'.toLowerCase()
-    }
-}
+const result = document.querySelector('.results');
+const headerResults = document.createElement('h2');
+const scoreDisplay = document.querySelector('h3');
+
+let playerSelection;
+let roundCPU;
+let score = 0;
 
 function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt(`Your Choice: Rock, Paper, Scissors`).toLowerCase()
-        if(playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
-            console.log("SELECT A CHOICE");
-            i--;
-            continue;
-        }
-        const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection))
+    function playerPlay() {
+        document.querySelector("#btn1").addEventListener('click', () => playerSelection = 'rock')
+        document.querySelector("#btn2").addEventListener('click', () => playerSelection = 'paper')
+        document.querySelector("#btn3").addEventListener('click', () => playerSelection = 'scissors')
     }
-}
 
-game();
+    function computerPlay() {
+        const choices = ['rock', 'paper', 'scissors'];
+        const selectChoice = Math.floor(Math.random() * 3);
+        roundCPU = (choices[selectChoice]);
+    }
+
+    function playRound(roundCPU, playerSelection) {
+        if (playerSelection == 'rock' && roundCPU == 'scissors') {
+            score += 1;
+            headerResults.textContent = `You Win this Round!`
+
+            scoreDisplay.textContent = `Score: ${score}`
+            result.append(headerResults);
+        } else if (playerSelection == 'paper' && roundCPU == 'rock') {
+            score += 1;
+            headerResults.textContent = `You Win this Round!`
+            scoreDisplay.textContent = `Score: ${score}`
+            result.append(headerResults);
+        } else if (playerSelection == 'scissors' && roundCPU == 'paper') {
+            score += 1;
+            headerResults.textContent = `You Win this Round!`
+            scoreDisplay.textContent = `Score: ${score}`
+            result.append(headerResults);
+        } else if (playerSelection == roundCPU) {
+            headerResults.textContent = 'The Round is a Tie!';
+            scoreDisplay.textContent = `Score: ${score}`
+            result.append(headerResults);
+        } else {
+            headerResults.textContent = `You lose this Round!`;
+            scoreDisplay.textContent = `Score: ${score}`
+            result.append(headerResults);
+        }
+
+    }
+    function checkScore() {
+        if (score == 5) {
+            alert("Game Over!")
+            headerResults.textContent = ""
+            score = 0;
+        }
+    }
+    playerPlay();
+    computerPlay();
+    playRound(roundCPU, playerSelection);
+    checkScore();
+}
